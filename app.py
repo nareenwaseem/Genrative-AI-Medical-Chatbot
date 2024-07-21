d3e9b8c4f227a9fc633c77b650ffaaa8dab0e224
Nareen Waseem <113854962+nareenwaseem@users.noreply.github.com>
2024-07-22 00:18:06 +0500

    .

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ from langchain.chains import RetrievalQA
 from dotenv import load_dotenv
 from src.prompt import *
 import os
-from pinecone import Pinecone, ServerlessSpec
 from langchain.vectorstores import Pinecone as PC
-from langchain_pinecone import Pinecone
 from langchain_pinecone import PineconeVectorStore
+from langchain.chains.retrieval_qa.base import BaseRetriever
+
+
 
 app = Flask(__name__)
 
@@ -18,49 +19,56 @@ load_dotenv()
 
 PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
 
+# Download embeddings
 embeddings = download_hugging_face_embeddings()
+print(f"Embeddings downloaded: {embeddings}")
 
-index_name="medical-chatbot-1"
+# Define the index name
+index_name = "medical-chatbot-2"
 
-#Loading the index
-docsearch=PineconeVectorStore.from_existing_index(index_name, embeddings)
+# Load the index
+docsearch = PineconeVectorStore.from_existing_index(index_name, embeddings)
+print(f"Docsearch initialized: {docsearch}")
 
+# Define the prompt template
+PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+print(f"Prompt template created: {PROMPT}")
 
-PROMPT=PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+# Define the language model
+llm = CTransformers(model="model/llama-2-7b-chat.ggmlv3.q4_0.bin",
+                    model_type="llama",
+                    config={'max_new_tokens': 512,
+                            'temperature': 0.8})
+print(f"Language model initialized: {llm}")
 
-chain_type_kwargs={"prompt": PROMPT}
+# Define chain type kwargs
+chain_type_kwargs = {"prompt": PROMPT}
 
-llm=CTransformers(model="model/llama-2-7b-chat.ggmlv3.q4_0.bin",
-                  model_type="llama",
-                  config={'max_new_tokens':512,
-                          'temperature':0.8})
+retriever = docsearch.as_retriever(search_kwargs={"k": 2}
+)
 
-
-qa=RetrievalQA.from_chain_type(
+# Initialize the QA chain
+qa = RetrievalQA.from_chain_type(
     llm=llm, 
     chain_type="stuff", 
-    retriever=docsearch.as_retriever(search_kwargs={'k': 2}),
-    return_source_documents=True, 
-    chain_type_kwargs=chain_type_kwargs)
-
-
+    chain_type_kwargs=chain_type_kwargs,
+    retriever=retriever,
+    return_source_documents=True
+)
+print(f"QA chain initialized: {qa}")
 
 @app.route("/")
 def index():
     return render_template('chat.html')
-
-
 
 @app.route("/get", methods=["GET", "POST"])
 def chat():
     msg = request.form["msg"]
     input = msg
     print(input)
-    result=qa({"query": input})
+    result = qa({"query": input})
     print("Response : ", result["result"])
     return str(result["result"])
 
-
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
+    app.run(host="0.0.0.0", port=8060, debug=True)
--- store_index.py
+++ store_index.py
@@ -12,12 +12,13 @@ from langchain_pinecone import PineconeVectorStore
 load_dotenv()
 
 PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
 
 extracted_data = load_pdf("data/")
 text_chunks = text_split(extracted_data)
 embeddings = download_hugging_face_embeddings()
 
-index_name = "medical-chatbot-1"
+index_name = "medical-chatbot-2"
 docs_chunks = [t.page_content for t in text_chunks]
 vectorstore = PineconeVectorStore.from_texts(
     docs_chunks,
